60d0b54c7e525abb37b78d178e676c751f6b4127
ByronGC <67564312+ByronGC@users.noreply.github.com>
2023-06-25 16:47:12 -0600

    Delete claseCliente.py

--- claseCliente.py
+++
@@ -1,9 +0,0 @@
-from hashTableStr import libros
-
-class Cliente:
-    def __init__(self, nombre, id, direccion, telefono):
-        self.nombre = nombre
-        self.id = id   
-        self.direccion = direccion
-        self.telefono = telefono  
-        self.pedidos = []
